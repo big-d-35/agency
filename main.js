64be5a271e42991df3a70b0c8ffbfdd9d8fc187d
big-d-35 <rdn.perfection@icloud.com>
2019-01-12 23:43:21 +0300

    js button active

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
+const headerHomeButton = document.getElementById('header-home-button');
+
 const webDevButton = document.getElementById('button-webdev');
 const videoButton = document.getElementById('button-video');
 const designButton = document.getElementById('button-design');
 
-
-
+const mainPage = document.getElementById('main-page');
+const webDevPage = document.getElementById('web-dev-page');
+const videoPage = document.getElementById('video-page');
+const designPage = document.getElementById('design-page');
+const whyPage = document.getElementById('why-page');
 
 //preloader
 window.onload = setTimeout(function(){
@@ -11,3 +16,34 @@ window.onload = setTimeout(function(){
  }, 3900);
 
 //preloader
+
+headerHomeButton.onclick = function() {
+    
+    webDevPage.classList.remove('active');
+    videoPage.classList.remove('active');
+    designPage.classList.remove('active');
+    
+    mainPage.classList.add('active');
+}
+
+
+webDevButton.onclick = function() {
+    
+    mainPage.classList.remove('active');
+    
+    webDevPage.classList.add('active');
+}
+
+videoButton.onclick = function() {
+    
+    mainPage.classList.remove('active');
+    
+    videoPage.classList.add('active');
+}
+
+designButton.onclick = function() {
+    
+    mainPage.classList.remove('active');
+    
+    designPage.classList.add('active');
+}
